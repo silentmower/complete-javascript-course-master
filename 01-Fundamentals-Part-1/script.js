e9cd0e922223e6bcff1f351cf2b12751485d57dc
filip <filips.zawada@gmail.com>
2025-06-19 00:35:16 +0200

    js

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -13,17 +13,55 @@
 // //coment
 
 // let amountOfMoney = 1000;
-// console.log(typeof amountOfMoney)
+// console.log(typeof amountOfMoney);
 
 // amountOfMoney = "tausend";
-// console.log(typeof amountOfMoney)
+// console.log(typeof amountOfMoney);
 
 // amountOfMoney = false;
-// console.log(typeof amountOfMoney)
+// console.log(typeof amountOfMoney);
 
-// console.log(typeof null)
+// console.log(typeof null);
 
 // const birthYear = 2004;
 // console.log(birthYear);
 
 // birthYear = 2000; 
+
+// let now = 2025;
+// const ageJanek = now - 2003;
+// const ageAnia = now - 2010;
+// console.log(ageJanek, ageAnia);
+
+// console.log(ageJanek * 2, ageAnia / 3);
+
+// const firstName = 'Janek';
+// const lasteName = 'Siemaneczko';
+// console.log(firstName + " " + lasteName);
+
+// let x = 10 +5;
+// x += 10;
+// console.log(x);
+// x /= 20;
+// x++;
+// console.log(x);
+
+// console.log(ageJanek > ageAnia);
+// console.log(ageAnia >= 18);
+
+// const isFullYear = ageAnia >= 18;
+
+const now = 2025;
+const ageJanek = now - 1995;
+const ageAnia = now - 2007;
+
+console.log(now - 1995 > now - 2007);
+
+let x,y;
+x = y = 25 - 10 - 5;
+console.log(x,y);
+
+const averageAge = (ageJanek + ageAnia) / 2;
+console.log(ageJanek, ageAnia, averageAge);
+
+
