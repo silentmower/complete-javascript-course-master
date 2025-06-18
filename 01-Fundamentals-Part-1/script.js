929b7149b85c5da23164b94a33ac85ac5a86a4f0
filip <filips.zawada@gmail.com>
2025-06-18 22:16:08 +0200

    Initial commit

---
+++ 01-Fundamentals-Part-1/script.js
@@ -0,0 +1,25 @@
+// let js = "amazing";
+
+// console.log(40+8+23-10);
+
+// console.log("Janek");
+// console.log(23);
+
+// let firstName = "Janek"
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+
+// //coment
+
+// let amountOfMoney = 1000;
+// console.log(typeof amountOfMoney)
+
+// amountOfMoney = "tausend";
+// console.log(typeof amountOfMoney)
+
+// amountOfMoney = false;
+// console.log(typeof amountOfMoney)
+
+// console.log(typeof null)
+
