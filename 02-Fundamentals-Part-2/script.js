a540395b9e5700c58aa54983a15c5aefbc80a89c
filip <filips.zawada@gmail.com>
2025-06-20 23:48:36 +0200

    js

---
+++ 02-Fundamentals-Part-2/script.js
@@ -0,0 +1,65 @@
+// 'use strict';
+
+// const hasDriersLicence = true;
+
+// // if (hasDrierLicence) console.log("can drive");
+// if (hasDriersLicence) console.log("can drive");
+
+// // const interface = "music";
+
+// function fruitProcessor(apples, oranges){
+//     const jucie = `Juice with ${apples} apples and ${oranges} oranges.`;
+//     return jucie;
+// }
+
+// const appleOrangeJucie = fruitProcessor(10, 2);
+// console.log(appleOrangeJucie);
+
+// const appleOrangeJucie2 = fruitProcessor(1, 3);
+// console.log(appleOrangeJucie2);
+
+//declaration
+
+// function calcAge1(birthYear){
+//     return 2025 - birthYear;
+// }
+
+// const age1 = calcAge1(1995);
+
+// const calcAge2 = function(birthYear){
+//     return 2025 - birthYear;
+// }
+
+// const age2 = calcAge2(1995);
+
+// console.log(age1, age2);
+
+
+// const calcAge3 = birthYear => 2025 - birthYear;
+
+// console.log(calcAge3(1995));
+
+
+// const yearsUntilRetirement = (birthYear, firstname) => {
+//     const age = 2025 - birthYear;
+//     const retirement = 65 - age;
+//     return `${firstname} retires in ${retirement} years`;
+// }
+
+// console.log(yearsUntilRetirement(1995, "Filip"));
+
+// const cutFruit = function(fruit) {
+//     return fruit * 4;
+// }
+
+
+// function fruitProcessor(apples, oranges){
+//     const applePieces = cutFruit(apples);
+//     const orangePieces = cutFruit(oranges);
+
+//     const jucie = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of noranges.`;
+//     return jucie;
+// }
+
+// console.log(fruitProcessor(2,3));
+
