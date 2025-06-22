d326c8d34e79e864bd0d47b8c4506fd5c17e48ad
silentmower <filips.zawada@gmail.com>
2025-06-22 21:01:46 +0200

    Test push

--- 03-Developer-Skills/script.js
+++ 03-Developer-Skills/script.js
@@ -9,27 +9,37 @@
 
 // console.log(calcAge(2004));
 
-const temperatures1 = [3 - 2, -6, -1, 'error', 9, 13, 17, 15, 9, 5];
+// const temperatures1 = [3 - 2, -6, -1, 'error', 9, 13, 17, 15, 9, 5];
 
-const temperatures2 = [2, -7, 'error', 12, 0, 3, 10];
+// const temperatures2 = [2, -7, 'error', 12, 0, 3, 10];
 
-const calcTempAmplitude = function (t1, t2) {
-  const temps = t1.concat(t2);
-  console.log(temps);
+// const calcTempAmplitude = function (t1, t2) {
+//   const temps = t1.concat(t2);
+//   console.log(temps);
 
-  let tempMax = temps[0];
-  let tempMin = temps[0];
+//   let tempMax = temps[0];
+//   let tempMin = temps[0];
 
-  for (let i = 1; i < temps.length; i++) {
-    let current = temps[i];
-    if (current > tempMax) tempMax = current;
-    if (current < tempMin) tempMin = current;
-  }
+//   for (let i = 1; i < temps.length; i++) {
+//     let current = temps[i];
+//     if (current > tempMax) tempMax = current;
+//     if (current < tempMin) tempMin = current;
+//   }
 
-  console.log(tempMax, tempMin);
+//   console.log(tempMax, tempMin);
 
-  return tempMax - tempMin;
-};
+//   return tempMax - tempMin;
+// };
 
-const amplitude = calcTempAmplitude(temperatures1, temperatures2);
-console.log(amplitude);
+// const amplitude = calcTempAmplitude(temperatures1, temperatures2);
+// console.log(amplitude);
+
+let num = Number(prompt('podaj liczbe'));
+let promptNumbers = [];
+
+while (num !== 0) {
+  promptNumbers.push(num);
+  num = Number(prompt('podaj liczbe'));
+}
+
+console.log(promptNumbers);
