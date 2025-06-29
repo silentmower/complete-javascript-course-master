590b02e92eeb9b49bf63c0e7b47933f3613f5652
filip <filips.zawada@gmail.com>
2025-06-29 23:41:54 +0200

    project#3

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -54,7 +54,7 @@ btnHold.addEventListener("click", function () {
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
 
-    if (scores[activePlayer] >= 10) {
+    if (scores[activePlayer] >= 100) {
       playing = false;
       diceEl.classList.add("hidden");
       document
